5b94f5df865e4f49f363b5d5eb66655a5e0a3fa8
tornike_totl <ttotl18@freeuni.edu.ge>
2021-07-10 00:17:08 +0400

    Add some methods to Chat interface

--- src/main/java/model/Chat.java
+++ src/main/java/model/Chat.java
@@ -1,4 +1,9 @@
 package model;
 
 public interface Chat {
+    void addMember(Account account);
+    void removeMember(Account account);
+    
+    void sendMessage(String message);
+    void sendPhoto(Image image); // don't know how Image works :D
 }
