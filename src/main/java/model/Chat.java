2ca26677de495ed1362f467ce9979fd0a352575d
tornike_totl <ttotl18@freeuni.edu.ge>
2021-07-10 02:23:31 +0400

    Change Chat interface & Create Message interface
    Closes #14

--- src/main/java/model/Chat.java
+++ src/main/java/model/Chat.java
@@ -4,6 +4,5 @@ public interface Chat {
     void addMember(Account account);
     void removeMember(Account account);
     
-    void sendMessage(String message);
-    void sendPhoto(Image image); // don't know how Image works :D
+    void sendMessage(Message message);
 }
