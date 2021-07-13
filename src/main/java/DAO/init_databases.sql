3b4a56024c33bdf145fe741ea42834e7ed69cd11
Shughla <nikusha1702@outlook.com>
2021-07-13 13:46:04 +0400

    Add db schema for messages/chats

--- src/main/java/DAO/init_databases.sql
+++ src/main/java/DAO/init_databases.sql
@@ -13,3 +13,28 @@ CREATE TABLE IF NOT EXISTS accounts (
     `pass` BLOB(64) NOT NULL,
      FOREIGN KEY (`location_id`) REFERENCES locations(`location_id`)
 );
+
+# single chat with multiple chat users
+CREATE TABLE IF NOT EXISTS chat (
+    `is_private` BOOL NOT NULL,
+    # communicates with chat users and message db with this
+    `chat_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY NOT NULL
+);
+
+# chat users for chat db
+CREATE TABLE IF NOT EXISTS chat_users (
+    `chat_id` INT NOT NULL,
+    `account_mail` VARCHAR(64) NOT NULL,
+    FOREIGN KEY (`chat_id`) REFERENCES  chat(`chat_id`),
+    FOREIGN KEY (`account_mail`) REFERENCES accounts(`mail`)
+);
+
+# messages for chat
+CREATE TABLE IF NOT EXISTS message (
+    `chat_id` INT NOT NULL,
+    `is_picture` BOOL NOT NULL,
+    `message` VARCHAR(64) NOT NULL,
+    `sender_mail` VARCHAR(64) NOT NULL,
+    FOREIGN KEY (`chat_id`) REFERENCES chat(`chat_id`),
+    FOREIGN KEY (`sender_mail`) REFERENCES accounts(`mail`)
+);
