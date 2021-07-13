24bb489db748e46d9d423c6ec7d028fa28a2d14c
Shughla <nikusha1702@outlook.com>
2021-07-13 22:05:39 +0400

    Add profile servlet

--- src/main/java/web/ProfileServlet.java
+++ src/main/java/web/ProfileServlet.java
@@ -1,14 +1,28 @@
 package web;
 
+import DAO.AccountsStore;
+import model.Account;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
 
 public class ProfileServlet extends HttpServlet {
+    private static final int uLen = "id=".length();
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
-        if(req.getQueryString()== null || req.getQueryString().length()<3)  return;
-        String id = req.getQueryString().substring(3);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if(req.getQueryString()== null || req.getQueryString().length()<uLen)  return; // wrong url
+        String mail = req.getQueryString().substring(uLen);
+        Account current = (Account)req.getAttribute("current-account");
+        if(current.getMail().equals(mail)){
+            req.getRequestDispatcher("profile.jsp").forward(req,resp);
+        }else{
+            AccountsStore as = (AccountsStore) req.getServletContext().getAttribute("accounts-store");
+            Account accToShow = as.getAccount("mail");
+            // ეს ექაუნთი უნდა აჩვენო ლევან, რო ნახავ ეს კომენტარი წაშალე
+            req.setAttribute("profile-account",accToShow);
+            req.getRequestDispatcher("profileOther.jsp").forward(req,resp);
+        }
 
     }
 
---
+++ src/main/webapp/profileOther.jsp
@@ -0,0 +1,10 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Someone else's profile</title>
+</head>
+<body>
+
+</body>
+</html>
