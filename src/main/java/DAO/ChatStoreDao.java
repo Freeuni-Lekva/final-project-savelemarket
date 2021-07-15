e6cec8208993f52beeb9160265cc28b6fb9a7c99
Shughla <nikusha1702@outlook.com>
2021-07-16 00:18:11 +0400

    Add some chat queries

--- src/main/java/DAO/ChatStoreDao.java
+++ src/main/java/DAO/ChatStoreDao.java
@@ -5,10 +5,7 @@ import model.Message;
 
 import javax.sql.DataSource;
 import javax.xml.transform.Result;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.List;
 
 public class ChatStoreDao implements ChatStore{
@@ -16,25 +13,23 @@ public class ChatStoreDao implements ChatStore{
     private DataSource dataSource;
 
     //queries and updates
-    private static final String getPrivateChatID= "";
+    private static final String getPrivateChatID=
+            "select acc_1.chat_id from chat_users acc_1 inner join chat using (chat_id) join chat_users acc_2 " +
+                    "on acc_1.account_mail = ? and acc_2.account_mail = ? and acc_1.chat_id = acc_2.chat_id and is_private = true;";
     private static final String getPublicChatID= "";
     private static final String addMessage = "";
     private static final String addAccounts = "";
     private static final String createPublicChat = "";
     private static final String createPrivateChat = "INSERT INTO chat(is_private) VALUES(true);";
+    private static final String insertIntoChatUsers ="INSERT INTO chat_users(chat_id,account_mail) VALUES(?,?) ";
 
+    private static final int ID_DOESNT_EXIST = 0;
+    private static final int WRONG_ID = -1;
+    private static final int MORE_THAN_ONE_PRIVATE = -2;
     public ChatStoreDao(DataSource dataSource) {
         this.dataSource = dataSource;
     }
-    private PreparedStatement prepare(String str, int returnKey){
-        try {
-            if(returnKey == -1) return dataSource.getConnection().prepareStatement(str);
-            return dataSource.getConnection().prepareStatement(str,returnKey);
-        } catch (SQLException sqlException) {
-            sqlException.printStackTrace();
-        }
-        return null;
-    }
+
     private int getID(PreparedStatement st){
         try {
             ResultSet set = st.getGeneratedKeys();
@@ -44,55 +39,97 @@ public class ChatStoreDao implements ChatStore{
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         }
-        return -1;
+        return WRONG_ID;
     }
     @Override
     public int getPrivateChatID(Account sender, Account receiver) {
-        PreparedStatement st = prepare(getPrivateChatID,-1);
-        if(st == null) return -1;// shouldn't happen but oh well...
-        //do stuff here
-        return 0;
+        int id = ID_DOESNT_EXIST;
+        try {
+            PreparedStatement st = dataSource.getConnection().prepareStatement(getPrivateChatID);
+            st.setString(1,sender.getMail());
+            st.setString(2,receiver.getMail());
+            ResultSet resultSet = st.executeQuery();
+            if(resultSet.next()){
+                id = resultSet.getInt(1);
+            }
+            //returned more than 2 private chats, shouldn't happen
+            if(resultSet.next()){
+                System.out.println("------------------------------------- more than 1 private chats exist -------------------------------------");
+                id = MORE_THAN_ONE_PRIVATE;
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        return id;
     }
 
     //might change this one
     @Override
     public int getPublicChatID(List<Account> accounts) {
-        PreparedStatement st = prepare(getPublicChatID,-1);
-        if(st == null) return -1;
+        try {
+            PreparedStatement st = dataSource.getConnection().prepareStatement(getPublicChatID);
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
         //do stuff here
-        return 0;
+        return WRONG_ID;
     }
     @Override
     public void addMessage(Message message, int id) {
-        PreparedStatement st = prepare(addMessage,-1);
+        try {
+            PreparedStatement st = dataSource.getConnection().prepareStatement(addMessage);
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
     }
 
     @Override
     public void addAccounts(List<Account> accounts, int id) {
-        PreparedStatement st = prepare(addAccounts,-1);
+        try {
+            PreparedStatement st = dataSource.getConnection().prepareStatement(addAccounts);
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
     }
 
     @Override
     public int createPublicChat(List<Account> accounts) {
-        PreparedStatement st = prepare(createPublicChat,-1);
-        if(st == null) return -1;
-        //do stuff here
-        return 0;
-    }
-    private PreparedStatement stringSetter(PreparedStatement st, String str, int index){
-         return null;
+        /**
+         * need to change this so that we somehow keep track of what location has what ID, otherwise it's going to be pain
+         */
+        try {
+            PreparedStatement st = dataSource.getConnection().prepareStatement(createPublicChat);
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        return WRONG_ID;
     }
     @Override
     public int createPrivateChat(Account sender, Account receiver) {
-        PreparedStatement st = prepare(createPrivateChat, Statement.RETURN_GENERATED_KEYS);
-        int id = getID(st);
-        PreparedStatement st1 = prepare("INSERT INTO chat_users(chat_id,account_mail) VALUES(" + id + "?)",-1);
-        PreparedStatement st2 = prepare("INSERT INTO chat_users(chat_id,account_mail) VALUES(" + id + "?)",-1);
-//        st1.setString(1,sender.getMail());
-//        st2.setString(2,sender.getMail());
-
-        if(st == null) return -1;
-        //do stuff here
+        int id = WRONG_ID;
+        try {
+            Connection c = dataSource.getConnection();
+            //always creates new chat even if it existed before (needs change) needs
+            // to do getPrivateChatID before this.
+            int existingID = getPrivateChatID(sender,receiver);
+            if(existingID == ID_DOESNT_EXIST) {
+                PreparedStatement st = c.prepareStatement(createPrivateChat, Statement.RETURN_GENERATED_KEYS);
+                st.executeUpdate();
+                id = getID(st);
+                PreparedStatement st1 = c.prepareStatement(insertIntoChatUsers);
+                PreparedStatement st2 = c.prepareStatement(insertIntoChatUsers);
+                st1.setInt(1, id);
+                st1.setString(2, sender.getMail());
+                st2.setInt(1, id);
+                st2.setString(1, receiver.getMail());
+                st1.executeUpdate();
+                st2.executeUpdate();
+            }else{
+                return existingID;
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
         return id;
     }
 }
--- src/test/java/ChatStoreDaoTest.java
+++ src/test/java/ChatStoreDaoTest.java
@@ -1,9 +1,28 @@
+import DAO.AccountsStore;
+import DAO.AccountsStoreDao;
+import DAO.ChatStore;
+import DAO.ChatStoreDao;
+import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
+import model.Account;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ChatStoreDaoTest {
 
     @Test
     public void baseTest(){
-
+        MysqlConnectionPoolDataSource ds = new MysqlConnectionPoolDataSource();
+        ds.setServerName("localhost");
+        ds.setPort(3306);
+        ds.setDatabaseName("myDatabase");
+        ds.setUser("shug");
+        ds.setPassword("");
+        ChatStore chatStore = new ChatStoreDao(ds);
+        AccountsStore accStore = new AccountsStoreDao(ds);
+        Account sender = accStore.getAccount("mail");
+        Account receiver = accStore.getAccount("mail2");
+        int id = chatStore.createPrivateChat(sender,receiver);
+        assertTrue(id != -1);
     }
 }
