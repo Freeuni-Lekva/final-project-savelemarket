9c9db589e4a00cab00a5598c29a46a294b8c14e8
Shughla <nikusha1702@outlook.com>
2021-08-06 23:18:05 +0400

    Add to ChatServlet
    Fixed sending whitespace messages (only worked for 1 "",
    now it works for any number of whitespace)

--- src/main/java/web/ChatServlet.java
+++ src/main/java/web/ChatServlet.java
@@ -23,7 +23,7 @@ public class ChatServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         chatStore = (ChatStore) request.getServletContext().getAttribute("chat-store");
         String messageText = (String)(request.getParameter("user-message"));
-        if(messageText != "") {
+        if(messageText.trim().length() > 0) {
             Account current = (Account) request.getSession().getAttribute("current-account");
             Message message = new GeneralMessage(current, messageText, false, current.getLocation().getChatID());
             chatStore.addMessage(message);
