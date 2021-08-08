4da4190054a36837ea6f6b9256881853caccdb21
Shughla <nikusha1702@outlook.com>
2021-08-08 16:11:16 +0400

    Add status messages to requests

--- src/main/java/model/Notification.java
+++ src/main/java/model/Notification.java
@@ -4,11 +4,14 @@ public interface Notification {
     int PENDING = 0;
     int ACCEPTED = 1;
     int REJECTED = -1;
-
+    String PENDING_MSG = "Request is pending.";
+    String ACCEPTED_MSG = "Request has been accepted.";
+    String REJECTED_MSG = "Request has been declined.";
     int getStatus();
     String getSenderMail();
     String getReceiverMail();
     Location getRequestedLocation();
     double getPrice();
+    String getStatusMessage();
     int getNotificationID();
 }
--- src/main/java/model/RequestNotification.java
+++ src/main/java/model/RequestNotification.java
@@ -50,7 +50,11 @@ public class RequestNotification implements Notification{
     public double getPrice() {
         return price;
     }
-
+    @Override
+    public String getStatusMessage(){
+        if(status == Notification.PENDING) return PENDING_MSG;
+        return (status == Notification.ACCEPTED ? ACCEPTED_MSG : REJECTED_MSG);
+    }
     @Override
     public String toString() {
         return "RequestNotification{" +
