91bbcc7e2c2bbc20d607da5193b1fa04a8ba2532
tornike_totl <ttotl18@freeuni.edu.ge>
2021-08-10 13:53:42 +0400

    Add ManageNotificationsServlet implementation

--- src/main/java/web/ManageNotificationsServlet.java
+++ src/main/java/web/ManageNotificationsServlet.java
@@ -1,6 +1,11 @@
 package web;
 
+import DAO.AccountsStore;
+import DAO.LocationStore;
 import DAO.NotificationStore;
+import DAO.ShoppingStore;
+import model.Account;
+import model.Location;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -20,13 +25,33 @@ public class ManageNotificationsServlet extends GeneralServlet{
         redirectIfNotLogged(request,response);
         request.setCharacterEncoding("UTF-8");
 
+        AccountsStore accountsStore = getAccountsStoreDao(request);
+        LocationStore locationStore = getLocationStoreDao(request);
         NotificationStore notificationStore = getNotificationStoreDao(request);
-        String remove = request.getParameter("წაშლა");
-        String decline = request.getParameter("უარყოფა");
-        String accept = request.getParameter("დათანხმება");
-        System.out.println("remove = "+remove);
-        System.out.println("decline = "+decline);
-        System.out.println("accept = "+accept);
+        ShoppingStore shoppingStore = getShoppingStoreDao(request);;
+        Account currentAccount = getCurrentAccount(request);
+
+        String remove = request.getParameter("remove");
+        String decline = request.getParameter("decline");
+        String accept = request.getParameter("accept");
+
+        String senderMail = request.getParameter("sender-mail");
+
+        int notificationId = Integer.parseInt(request.getParameter("notif_id"));
+        if(remove != null){
+            notificationStore.deleteNotification(notificationId);
+        } else if(decline != null){
+            notificationStore.rejectNotification(notificationId);
+        } else if(accept != null){
+            notificationStore.acceptNotification(notificationId);
+            Account senderAccount = accountsStore.getAccount(senderMail); // შეტყობინების გამომგზავნი ტიპი
+            Location suggestedLocation = locationStore.getLocation(senderMail); // გამომგზავნის მიერ შემოთავაზებული ლოკაცია
+            Location requiredLocation = currentAccount.getLocation(); // გამომგზავნს რა ლოკაციაც უნდა
+            accountsStore.updateLocation(currentAccount, suggestedLocation);
+            accountsStore.updateLocation(senderAccount, requiredLocation);
+            shoppingStore.removeAllItemFor(currentAccount.getMail());
+            shoppingStore.removeAllItemFor(senderMail);
+        }
         response.sendRedirect("/manage-notifications");
     }
 }
